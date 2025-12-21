79b13c1a46d1402611a660354ee29341ba149ad7
Diksha karna <dikshakarna071@gmail.com>
2025-12-21 13:11:00 +0545

    login page comitted

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+     <div class="box glass">
+    <h2>Welcome</h2>
+    <p>Login to continue your skincare analysis</p>
+
+    <form action="backend/login.php" method="POST">
+        <input type="email" name="email" placeholder="Enter Email" required></br>
+        <input type="password" name="password" placeholder="Enter Password" required></br>
+        <button>Login</button>
+        <p>New user ? <a href="register.php">Create Account</a></p>
+    </form>
+</div>
 </body>
 </html>
